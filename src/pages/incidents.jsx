c9bde3b481083961bfcfde547041fd37a1452343
Joychandra13 <joychandra1313@gmail.com>
2024-10-19 02:13:17 +0600

    16

--- src/pages/incidents.jsx
+++ src/pages/incidents.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 function Incidents() {
 
   return (
-    <div className='p-5 md:p-0 font-Onest'>
-      <div className='bg-[#E4E4E780] overflow-hidden font-Onest p-5 md:p-0 border rounded-md md:border-none md:rounded-none'>
-      <div className='container md:flex md:mx-auto justify-between ms-[5%] items-center pt-[32px] pb-[23px] sm:p-3'>
+    <div>
+      <div className='bg-[#E4E4E780] overflow-hidden font-Onest'>
+      <div className='container md:flex md:mx-auto justify-between ms-[20%] items-center pt-[32px] pb-[23px] sm:p-3'>
       <div className='pb-[13px] md:p-0'>
         <p className='text-secondary'>Home - Incidents</p>
         <h1 className='text-[26px] font-bold font-Onest'>Incidents</h1>
